b506b8866c7561bd8a790cedf45cfe7e2debf52f
Diego Pronesti <dieh.diego@gmail.com>
2019-10-13 10:20:33 -0300

    save score function

--- routes/api/score.js
+++ routes/api/score.js
@@ -6,14 +6,14 @@ const admin = require('firebase-admin');
 const Score = require('../../models/Score');
 
 router.post('/test', (req, res) => {
-  console.log(req.body)
-})
+  console.log(req.body);
+});
 
 // @route GET api/score
 // @desc Get All scores by date
 // @access Public
 router.get('/recent_score', (req, res) => {
-    Score.find()
+  Score.find()
     .sort({ date: -1 })
     .then(scores => res.send(res.json(scores)));
 });
@@ -22,7 +22,7 @@ router.get('/recent_score', (req, res) => {
 // @desc Get All scores by highest
 // @access Public
 router.get('/high_score', (req, res) => {
-    Score.find()
+  Score.find()
     .sort({ score: 1 })
     .then(scores => res.send(res.json(scores)));
 });
@@ -31,8 +31,8 @@ router.get('/high_score', (req, res) => {
 // @desc Get All scores from one game in ascendent order
 // @access Public
 router.get('/from_game', (req, res) => {
-    const {game} = req.body;
-    Score.findOne({game})
+  const { game } = req.body;
+  Score.findOne({ game })
     .sort({ score: 1 })
     .then(scores => res.send(res.json(scores)));
 });
@@ -41,8 +41,8 @@ router.get('/from_game', (req, res) => {
 // @desc Get All scores from one user in ascendent order
 // @access Public
 router.get('/from_user', (req, res) => {
-    const {uid} = req.body;
-    Score.findOne({uid})
+  const { uid } = req.body;
+  Score.findOne({ uid })
     .sort({ score: 1 })
     .then(scores => res.send(res.json(scores)));
 });
@@ -50,27 +50,32 @@ router.get('/from_user', (req, res) => {
 // @route   POST api/score
 // @desc    Register new score
 // @access  Public
-router.post('/save_score', (req, res) => {
-  const { uid, game, score} = req.body;
+router.post('/save', (req, res) => {
+  const { uid, game, score } = req.body;
 
   // Simple validation
-  if(!uid || !game || !score) {
+  if (!uid || !game || !score) {
     return res.status(400).json({ msg: 'Please enter all fields' });
   }
 
-  // Check for existing user
-  Score.findOne({ score })
-    .then(user => {
-      if(user) return res.status(400).json({ msg: 'User already exists' });
-      
-          const newScore = new Score({
-            uid: userRecord.uid,
-            game,
-            score,
-          });
+  const newScore = new Score({
+    uid,
+    game,
+    score
+  });
 
-          newScore.save();
-        });
+  // Check for existing user
+  Score.findOne({ uid, game }).then(sameidandgame => {
+    console.log(sameidandgame);
+    if (sameidandgame) {
+      if (score > sameidandgame.score) {
+        newScore.save();
+      }
+      return res.status(400).json({ msg: 'User already have an higher score' });
+    } else {
+      newScore.save();
+    }
+  });
 });
 
 module.exports = router;
